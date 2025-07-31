ef941d58c382dbb69eb80fdc56181c2de0a60fec
Luk3y595 <mankey595595@gmail.com>
2025-08-01 09:26:15 +1200

    Bowls for pizza dough finished
    Bowls have been made so on click they will empty

---
+++ super-pizza-boy/Scenes/Bowl_Change_Flour.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var _flour = $AnimatedSprite2D
+
+func play_start_animation():
+	_flour.pause()
+
+func _on_input_event_flour(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		_flour.play()
--- super-pizza-boy/Scenes/Make_Bowl_Change.gd
+++ super-pizza-boy/Scenes/Make_Bowl_Change.gd
@@ -1,13 +1,10 @@
 extends Area2D
 
-@onready var _Water = get_node("../Full_Bowl_Of_Water")
-@onready var _Empty_Water = get_node("../Empty_Bowl_Of_Water")
+@onready var _water = $AnimatedSprite2D
 
-func _ready() -> void:
-	get_node(_Water).visible = true
-	get_node(_Empty_Water).visible = false
+func play_start_animation():
+	_water.pause()
 
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		get_node(_Water).visible = false
-		get_node(_Empty_Water).visible = true
+		_water.play()
